bffda04ded0804ee0196f9543cdda4ad9f2ecb8b
Jinglong Shen <sjinglong31@163.com>
2021-07-02 10:50:01 +0800

    add

--- scripts/offline_installation.sh
+++ scripts/offline_installation.sh
@@ -18,6 +18,14 @@ apt install -y python3-pip
 
 service docker start
 
+curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+sudo apt-get install apt-transport-https --yes
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
+
+helm plugin install https://github.com/chartmuseum/helm-push
+
 cd /tmp
 git clone https://gerrit.onap.org/r/oom/offline-installer onap-offline
 cd onap-offline
