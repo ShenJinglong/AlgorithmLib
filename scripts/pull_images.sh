5e9b04351f9b911331523b01cf66d258d55f46dd
Jinglong Shen <sjinglong31@163.com>
2021-07-02 23:15:44 +0800

    hh

--- scripts/pull_images.sh
+++ scripts/pull_images.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 images=(
-    kube-apiserver:v1.20.1
-    kube-controller-manager:v1.20.1
-    kube-scheduler:v1.20.1
-    kube-proxy:v1.20.1
-    pause:3.2
+    kube-apiserver:v1.21.2
+    kube-controller-manager:v1.21.2
+    kube-scheduler:v1.21.2
+    kube-proxy:v1.21.2
+    pause:3.4.1
     etcd:3.4.13-0
 )
 for imageName in ${images[@]} ; do
@@ -13,6 +13,6 @@ for imageName in ${images[@]} ; do
     docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/$imageName k8s.gcr.io/$imageName
     docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/$imageName
 done
-docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/coredns:1.7.0
-docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/coredns:1.7.0 k8s.gcr.io/coredns/coredns:v1.7.0
-docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/coredns:1.7.0
+docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/coredns:1.8.0
+docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/coredns:1.8.0 k8s.gcr.io/coredns/coredns:v1.8.0
+docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/coredns:1.8.0
