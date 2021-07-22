2037b4a51079cd520518ac20a11c44cbef73894a
Jinglong Shen <sjinglong31@163.com>
2021-07-22 16:00:09 +0800

    add kubernetes pubkey

--- scripts/onap/master1.sh
+++ scripts/onap/master1.sh
@@ -24,11 +24,11 @@ cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
 deb http://mirrors.ustc.edu.cn/kubernetes/apt kubernetes-xenial main
 EOF
 apt-get update
-read -p "Enter PUBKEY in the log above (2 items): " pubkey1 pubkey2
-gpg --keyserver keyserver.ubuntu.com --recv-keys $pubkey1
-gpg --keyserver keyserver.ubuntu.com --recv-keys $pubkey2
-gpg --export --armor $pubkey1 | apt-key add -
-gpg --export --armor $pubkey2 | apt-key add -
+# read -p "Enter PUBKEY in the log above (2 items): " pubkey1 pubkey2
+gpg --keyserver keyserver.ubuntu.com --recv-keys 307EA071
+gpg --keyserver keyserver.ubuntu.com --recv-keys 836F4BEB
+gpg --export --armor 307EA071 | apt-key add -
+gpg --export --armor 836F4BEB | apt-key add -
 apt-get update
 apt-get install -y kubelet kubeadm kubectl
 apt-mark hold kubelet kubeadm kubectl
